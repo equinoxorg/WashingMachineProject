7337520e4771a2621644afa3bc38d977ec94ba2a
LeonardoIalongo <leonardo.ialongo12@imperial.ac.uk>
2016-03-09 22:15:49 +0000

    Simple testing script for the optimisation algorithm

--- Cost Modelling/test_opt.m
+++ Cost Modelling/test_opt.m
@@ -1,2 +1,12 @@
 % Testing the optimisation function
 
+% Define parameters for the estimation
+E_cycle = 1.6;          % kWh per cycle
+D = 1*30* E_cycle;      % Demand
+eta_c = 0.7;            % Efficiency charging
+eta_d = 0.8;            % Efficiency discharging
+J_P = 840;              % Cost per kW nominal power
+J_C = 367;              % Cost per kWh nominal capacity
+
+
+[P, C, fail_rate] = opt_elect_sizing(D, eta_c, eta_d, J_P, J_C);
